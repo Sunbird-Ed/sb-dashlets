5ce1aa5457e46af970aa880036d783798f9719bc
Ravinder Kumar <yravinderkumar33@gmail.com>
2021-07-07 12:02:58 +0530

    chart types mapping datastructure change

--- projects/sb-dashlets/src/lib/components/dashlet/type_to_component_mapping.ts
+++ projects/sb-dashlets/src/lib/components/dashlet/type_to_component_mapping.ts
@@ -1,16 +1,16 @@
 import { ChartType, TableType } from '../../types/index';
 
 export const TYPE_TO_COMPONENT_MAPPING = {
-  [ChartType.LINE]: () => import('../chart-js/chart-js.component').then(module => module.ChartJsComponent),
-  [ChartType.BAR]: () => import('../chart-js/chart-js.component').then(module => module.ChartJsComponent),
-  [ChartType.PIE]: () => import('../chart-js/chart-js.component').then(module => module.ChartJsComponent),
-  [ChartType.AREA]: () => import('../chart-js/chart-js.component').then(module => module.ChartJsComponent),
-  [ChartType.BUBBLE]: () => import('../chart-js/chart-js.component').then(module => module.ChartJsComponent),
-  [ChartType.DOUGHNUT]: () => import('../chart-js/chart-js.component').then(module => module.ChartJsComponent),
-  [ChartType.POLAR]: () => import('../chart-js/chart-js.component').then(module => module.ChartJsComponent),
-  [ChartType.SCATTER]: () => import('../chart-js/chart-js.component').then(module => module.ChartJsComponent),
-  [ChartType.DOUGHNUT]: () => import('../chart-js/chart-js.component').then(module => module.ChartJsComponent),
-  [ChartType.BIG_NUMBER]: () => import('../big-number/big-number.component').then(module => module.BigNumberComponent),
-  [TableType.TABLE]: () => import('../dt-table/dt-table.component').then(module => module.DtTableComponent),
-  [ChartType.MAP]: () => import('../map/map.component').then(module => module.MapComponent),
+  [ChartType.LINE]: { path: import('../chart-js/chart-js.component').then(module => module.ChartJsComponent) },
+  [ChartType.BAR]: { path: import('../chart-js/chart-js.component').then(module => module.ChartJsComponent) },
+  [ChartType.PIE]: { path: import('../chart-js/chart-js.component').then(module => module.ChartJsComponent) },
+  [ChartType.AREA]: { path: import('../chart-js/chart-js.component').then(module => module.ChartJsComponent) },
+  [ChartType.BUBBLE]: { path: import('../chart-js/chart-js.component').then(module => module.ChartJsComponent) },
+  [ChartType.DOUGHNUT]: { path: import('../chart-js/chart-js.component').then(module => module.ChartJsComponent) },
+  [ChartType.POLAR]: { path: import('../chart-js/chart-js.component').then(module => module.ChartJsComponent) },
+  [ChartType.SCATTER]: { path: import('../chart-js/chart-js.component').then(module => module.ChartJsComponent) },
+  [ChartType.DOUGHNUT]: { path: import('../chart-js/chart-js.component').then(module => module.ChartJsComponent) },
+  [ChartType.BIG_NUMBER]: { path: import('../big-number/big-number.component').then(module => module.BigNumberComponent) },
+  [TableType.TABLE]: { path: import('../dt-table/dt-table.component').then(module => module.DtTableComponent) },
+  [ChartType.MAP]: { path: import('../map/map.component').then(module => module.MapComponent) }
 };

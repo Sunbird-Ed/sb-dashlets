d008cb252d6b1fb7ed151c88b958d713dd63987e
princegupta1131 <114015020+princegupta1131@users.noreply.github.com>
2023-05-22 11:19:20 +0530

    @angular/core migration - migration-v14-typed-forms
    As of Angular version 14, Forms model classes accept a type parameter, and existing usages must be opted out to preserve backwards-compatibility.

--- projects/sb-dashlets/src/lib/components/filters/filters.component.ts
+++ projects/sb-dashlets/src/lib/components/filters/filters.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit, Output, EventEmitter, OnDestroy, Inject, ViewChild, ElementRef } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { UntypedFormBuilder, UntypedFormGroup } from '@angular/forms';
 import { DEFAULT_CONFIG } from '../../tokens/index';
 import * as _ from 'lodash-es'
 import { debounceTime, distinctUntilChanged, takeUntil, map, tap, pairwise, startWith } from 'rxjs/operators';
@@ -56,7 +56,7 @@ export class FiltersComponent implements OnInit, OnDestroy {
 
   private _data;
 
-  public filtersFormGroup: FormGroup;
+  public filtersFormGroup: UntypedFormGroup;
   public filters: IFilterConfiguration[];
   public unsubscribe$ = new Subject<void>();
   public ranges = ranges;
@@ -65,7 +65,7 @@ export class FiltersComponent implements OnInit, OnDestroy {
 
   @ViewChild('datePickerForFilters') datepicker: ElementRef;
 
-  constructor(private fb: FormBuilder, @Inject(DEFAULT_CONFIG) private defaultConfig) { }
+  constructor(private fb: UntypedFormBuilder, @Inject(DEFAULT_CONFIG) private defaultConfig) { }
 
   ngOnInit() {
     this._data = this.data;

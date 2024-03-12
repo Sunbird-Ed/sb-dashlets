10377be8a520d32d93588a084999860457162f9b
mithun30052001 <lmithun01@gmail.com>
2024-03-12 12:57:53 +0530

    chartsmodule import is modified as ngchartsmodule to suit angular 16 upgrade

--- projects/sb-dashlets/src/lib/dashlet.module.ts
+++ projects/sb-dashlets/src/lib/dashlet.module.ts
@@ -2,7 +2,7 @@ import { ModuleWithProviders, NgModule } from '@angular/core';
 import { DashletComponent, ChartJsComponent, BigNumberComponent, DtTableComponent, FiltersComponent, MapComponent } from './components/index';
 import { ReportWrapperDirective, TemplateRefsDirective } from './directives/index';
 import { HttpClientModule } from '@angular/common/http'
-import { ChartsModule } from 'ng2-charts'
+import { NgChartsModule } from 'ng2-charts'
 import { CommonModule } from '@angular/common';
 import { DataTablesModule } from "angular-datatables";
 import { ReactiveFormsModule } from '@angular/forms';
@@ -16,7 +16,7 @@ import { DataService } from './services/index';
 
 @NgModule({
     declarations: [ChartJsComponent, DashletComponent, ReportWrapperDirective, BigNumberComponent, DtTableComponent, TemplateRefsDirective, FiltersComponent, MapComponent],
-    imports: [HttpClientModule, ChartsModule, CommonModule, DataTablesModule, ReactiveFormsModule, NgMultiSelectDropDownModule,
+    imports: [HttpClientModule, NgChartsModule, CommonModule, DataTablesModule, ReactiveFormsModule, NgMultiSelectDropDownModule,
         NgxDaterangepickerMd.forRoot(),
     ],
     exports: [DashletComponent, TemplateRefsDirective]
